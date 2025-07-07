f9ed29f465d38b01b28d924258e39838854bad48
Nagesh Prajapati <nagesh.prajapati@einfochips.com>
2025-07-07 18:12:51 +0530

    # Conflicts: #	app/src/main/java/com/nagesh/graphql/presentation/screens/ContinentsScreen.kt

--- app/src/main/java/com/nagesh/graphql/MainActivity.kt
+++ app/src/main/java/com/nagesh/graphql/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.nagesh.graphql.presentation.continents.ContinentsViewModel
 import com.nagesh.graphql.presentation.screens.ContinentsScreen
@@ -30,21 +27,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GrapghqlAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/nagesh/graphql/presentation/screens/ContinentsScreen.kt
+++ app/src/main/java/com/nagesh/graphql/presentation/screens/ContinentsScreen.kt
@@ -1,8 +1,10 @@
 package com.nagesh.graphql.presentation.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,6 +16,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.nagesh.graphql.presentation.continents.ContinentsViewModel
@@ -34,35 +38,53 @@ fun ContinentsScreen(modifier: Modifier = Modifier, viewModel: ContinentsViewMod
         }
     }
     uiState.data?.continents?.let { list->
+        val grouped = list.groupBy { it.name.first().toString() }
         LazyColumn(
             modifier =
                 modifier
                     .fillMaxSize()
                     .padding(12.dp),
-        ) {
-            items(list) {it->
-                Card(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(10.dp)
-                ) {
-                    Column(
-                        modifier = Modifier.padding(12.dp),
+        )
+        {
+            grouped.forEach { key, value ->
+                stickyHeader {
+                    Box(
+                        modifier  = Modifier.fillMaxWidth()
+                            .padding(10.dp)
+                            .background(Color.LightGray),
                     ) {
                         Text(
-                            text = it.name,
-                            style = MaterialTheme.typography.headlineSmall,
-                        )
-                        Text(
-                            text = it.code,
-                            style = MaterialTheme.typography.bodyMedium,
+                            modifier  = Modifier.fillMaxWidth()
+                                .padding(10.dp),
+                            text = key,
+                            style = MaterialTheme.typography.headlineMedium,
+                            textAlign = TextAlign.Center
                         )
                     }
                 }
+                items(value) {it->
+
+                    Card(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(10.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(12.dp),
+                        ) {
+                            Text(
+                                text = it.name,
+                                style = MaterialTheme.typography.headlineSmall,
+                            )
+                            Text(
+                                text = it.code,
+                                style = MaterialTheme.typography.bodyMedium,
+                            )
+                        }
+                    }
+                }
             }
+
         }
     }
-
-
-
 }
